613957dfe07741c9d4758a8188708d861e8467e1
Harsh Chaudhary <harshchaudhary1123@gmail.com>
2022-06-13 19:30:21 +0530

    WIP

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { FetchService } from '../service/FetchService';
-import { Collapse } from 'antd';
+import {UpOutlined, DownOutlined} from '@ant-design/icons';
+
 
 const FAQ: React.FC = () => {
-
-const {Panel} = Collapse;
+const [opened, setOpened] = useState<any>(null);
 
 const [faqs, setFaqs] = useState<{question: string, answer: string}[]>();
 useEffect(() => {
@@ -16,6 +16,13 @@ useEffect(() => {
                }
              });
 }, []);
+
+const toggle = (i: any) => {
+     if (opened === i) {
+        return setOpened(null);
+     }
+     setOpened(i);
+ }
 
  return (
     <div className={'px-4 tab:px-4 laptop:px-20 py-16'}>
@@ -35,15 +42,23 @@ useEffect(() => {
             </div>
          </div>
          <div className={'w-full tab:pl-24'}>
-                <Collapse accordion expandIconPosition={"end"}>
-                  {faqs?.map((res, i) => {
-                     return (
-                        <Panel header={res.question} key={i}>
-                           <p>{res.answer}</p>
-                        </Panel>
-                     )
-                  })}
-                </Collapse>
+                {faqs?.map((res, i) => {
+                  return (
+                     <div className='flex flex-col space-y-3' key={i}>
+                        <div className='flex justify-between items-center cursor-pointer' onClick={() => toggle(i)}>
+                           <div className={'pr-2 font-semibold text-gray-subtitle'}>
+                           <span className={'font-hi'}>{res.question}</span>
+                           </div>
+                           <span>{opened === i ? <UpOutlined /> : <DownOutlined/>}</span>
+                        </div>
+                        <div className={opened === i ? 'answer visible' : 'answer'}>
+                           <div className={'pr-2 font-semibold text-rose-900'}>
+                              <span className={'font-hi'}>{res.answer}</span>
+                           </div>
+                        </div>
+                     </div>
+                  )
+                })}
          </div>
       </div>
     </div>
